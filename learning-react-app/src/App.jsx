e91f5cb0affc10725f5804ae725ac4a1a62da154
KavyaAdlakha <kavye14@gmail.com>
2025-07-18 19:35:16 +0530

    mini-project

--- learning-react-app/src/App.jsx
+++ learning-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Tittle from "./Tittle.jsx"
 import Product from "./Product.jsx" 
 import ProductTab from "./ProductTab.jsx"
+import Msgbox  from "./Msgbox.jsx";
 
 
 
@@ -9,6 +10,10 @@ function App() {
   return(
     <>
     <ProductTab/>
+    <Msgbox username="Kaavyy" textColor="aqua"/>
+    <Msgbox username="Kaavyy" textColor="yellow"/>
+    <Msgbox username="Kaavyy" textColor="pink "/>
+    
     </> 
     );
 }
--- learning-react-app/src/Product.jsx
+++ learning-react-app/src/Product.jsx
@@ -2,12 +2,13 @@ import "./Product.css"
 
 function Product({tittle,price ,features}){
     // const list = features.map((feature) => <li>{feature}</li>)
-    
+    let isDiscount = price>30000;
+    let styles = {backgroundColor: isDiscount? "aqua": "", color: isDiscount? "black":""};
     return(
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3 >{tittle}</h3>
             <h5>Price: {price}</h5>
-            {price>25000 && <p>Discount of 5% </p> }
+            {isDiscount && <p>Discount of 5% </p> }
             {/* <p>{list}</p> */}
         </div>
     );
