bdeb1f3ef467c9d5e3e4b7dc39d3b3a1bd59dca0
kavya <kavye14@gmail.com>
2025-07-31 01:43:55 +0530

    new

---
+++ nodejs/index.js
@@ -0,0 +1,20 @@
+// let n = 5;
+// for (let i=0; i<n; i++) {
+//    console.log("hello", "",i) ;
+// }
+
+// console.log("bye!")
+
+// console.log(process.argv);
+
+// let args = process.argv;
+// for (let i=2; i<args.length; i++) {
+//     console.log("hello & welcome", args[i])
+// }
+
+const math = require('./math')
+console.log(math.sum(5,6))
+console.log(math.mul(5,6))
+console.log(math.g)
+console.log(math.PI)
+
---
+++ nodejs/math.js
@@ -0,0 +1,17 @@
+module.exports.sum = (a,b) => a+b;
+module.exports.mul = (a,b) => a*b;
+module.exports.g = 9.8;
+module.exports.PI = 3.14;
+
+// module.exports.sum = sum;
+// module.exports.mul = mul;
+// module.exports.g = g;
+// module.exports.PI = PI;
+
+// let values = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// }
+// module.exports = values;
