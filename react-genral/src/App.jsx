a271d24512b91a81a1b90f875ee3f02e2ff01e25
KavyaAdlakha <kavye14@gmail.com>
2025-08-12 18:33:11 +0530

    complete

--- react-genral/src/App.jsx
+++ react-genral/src/App.jsx
@@ -3,6 +3,7 @@ import Form from './Forms/Form'
 import CommentsForm from './Forms/CommentsForm'
 import Counter from './Counter'
 import Joker from './Joker'
+import { useEffect, useState } from 'react'
 
 function App() {
 
--- react-genral/src/Joker.jsx
+++ react-genral/src/Joker.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
+
+
 export default function Joker() {
-    const URL = "https://official-joke-api.appspot.com/random_joke";
+  
+     const URL = "https://official-joke-api.appspot.com/random_joke";
 
     const getNewJoke = async () => {
       let response = await fetch(URL);
-      let jsonResponse = await re
-    }
+      let jsonResponse = await response.json()
+      setJoke({setup: jsonResponse.setup, punchline: jsonResponse.punchline})
+    };
+
+    useEffect(() => { 
+     async function getFirstJoke () {
+      let response = await fetch(URL);
+      let jsonResponse = await response.json()
+      console.log(jsonResponse);
+      setJoke({setup: jsonResponse.setup, punchline: jsonResponse.punchline})
+      } 
+      getFirstJoke(); 
+    },[]);
+    
+    let [joke, setJoke] = useState({});
+    
     return(
         <div>
             <h3>Joker!</h3>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
+            <button onClick={getNewJoke}>New Joke</button>
         </div>
     )
-}
+} 
