3e368c1ec537dc1e251371e8c37e1d04d30d1d52
KavyaAdlakha <kavye14@gmail.com>
2025-08-14 14:16:55 +0530

    CRUD

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require('path');
+const {v4: uuidv4} = require('uuid');
+const methodOverride = require("method-override");
+
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -12,16 +16,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+       id: uuidv4(), 
        username: "kaavyy" ,
-       content: "Studing MERN"
+       content: "Studying MERN"
     },
     {
+       id: uuidv4(),
        username: "kushal" ,
-       content: "Studing DSA"
+       content: "Studying DSA"
     },
     {
+       id: uuidv4(),
        username: "akshat" ,
-       content: "Studing Devops"
+       content: "Studying Devops"
     },
 ]
 
@@ -35,8 +42,29 @@ app.get("/posts/new", (req,res) => {
 
 app.post("/posts", (req,res) => {
     let {username, content} = req.body;
-    posts.push({username, content});
+    let id = uuidv4();
+    posts.push({id, username, content});
     res.redirect('/posts');
+})
+
+app.get("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render('show.ejs', {post});
+})
+
+app.patch("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let newContent = req.body.content
+    let post = posts.find((p) => id === p.id)
+    post.content = newContent;
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit", (req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id)
+    res.render('edit.ejs', {post});
 })
 
 app.listen(port, () => {
