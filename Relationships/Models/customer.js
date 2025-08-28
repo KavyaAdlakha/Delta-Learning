b7819049f16614a54bd7a4a0eecd4c49f4cdf62a
KavyaAdlakha <kavye14@gmail.com>
2025-08-28 11:38:30 +0530

    datbase relationship

---
+++ Relationships/Models/customer.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+main().then(() => console.log("connection successful")).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationdemo');
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price: Number,
+});
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+          type: Schema.Types.ObjectId, 
+          ref: "Order"   
+        },
+    ],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+// const addCustomer = async () => {
+//     let cust1 = new Customer({
+//         name: "Rahul Kumar",
+//     });
+
+//     let order1 = await Order.findOne({item:"chips"});
+//     let order2 = await Order.findOne({item:"chocolate"});
+
+//     cust1.orders.push(order1);
+//     cust1.orders.push(order2);
+
+//     let result = await cust1.save();
+//     console.log(result);
+// }
+
+// addCustomer();
+
+const findCustomer = async () => {
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+};
+
+findCustomer()
+
+// const addOrders = async () => {
+//     let res = await Order.insertMany([
+//         {item: "Somasa", price: 12},
+//         {item: "chips", price:10},
+//         {item: "chocolate", price: 40},
+//     ]);
+//     console.log(res);
+// };
+
+// addOrders();
